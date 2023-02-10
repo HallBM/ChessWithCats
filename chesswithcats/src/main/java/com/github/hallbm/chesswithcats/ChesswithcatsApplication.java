308db4484449afae723df2b5cd3fe8c8240937f0
HallBM <bhall.phd@gmail.com>
2023-02-10 14:39:49 -0500

    Init commit

---
+++ chesswithcats/src/main/java/com/github/hallbm/chesswithcats/ChesswithcatsApplication.java
@@ -0,0 +1,13 @@
+package com.github.hallbm.chesswithcats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesswithcatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChesswithcatsApplication.class, args);
+	}
+
+}
---
+++ chesswithcats/src/test/java/com/github/hallbm/chesswithcats/ChesswithcatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hallbm.chesswithcats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesswithcatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
