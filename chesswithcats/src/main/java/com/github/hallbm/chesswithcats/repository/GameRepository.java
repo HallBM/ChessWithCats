77e423f8c3f9073dcaa7ce20dd51a9d52f51887f
HallBM <bhall.phd@gmail.com>
2023-03-10 21:12:28 -0500

    Update GameRepository.java
    New methods for generating tables for user profile (Wins, Loss, Draw)

--- chesswithcats/src/main/java/com/github/hallbm/chesswithcats/repository/GameRepository.java
+++ chesswithcats/src/main/java/com/github/hallbm/chesswithcats/repository/GameRepository.java
@@ -4,14 +4,26 @@ package com.github.hallbm.chesswithcats.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.github.hallbm.chesswithcats.domain.GameEnums.GameOutcome;
+import com.github.hallbm.chesswithcats.domain.GameEnums.GameStyle;
 import com.github.hallbm.chesswithcats.model.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
+	
+	@Query(value = "SELECT * FROM games g "
+			+ "WHERE (g.white_username = :username OR g.black_username = :username) AND g.winner IS NULL "
+			+ "ORDER BY g.id", nativeQuery = true)
+	List<Game> findActiveByUsername(String username);
+	
+	@Query(value = "SELECT * FROM games g "
+			+ "WHERE (g.white_username = :username OR g.black_username = :username) AND g.winner IS NOT NULL "
+			+ "ORDER BY g.id", nativeQuery = true)
+	List<Game> findCompleteByUsername(String username);
 
-	public List<Game> findByWhiteUsernameOrBlackUsernameAndOutcomeOrderByStyleAscIdDesc(String white, String black, GameOutcome outcome);
-	public List<Game> findByWhiteUsernameOrBlackUsernameAndWinnerNotNullOrderByStyleAscIdDesc(String white, String black);
-
+	@Query(value = "SELECT g.style, g.outcome FROM games g "
+			+ "WHERE (g.white_username = :username1 AND g.black_username = :username2) OR "
+			+ "(g.white_username = username2 AND g.black_username = :username1)", nativeQuery = true)
+	List<Game> findCompleteByOpponents(String username1, String username2);
 
 }
