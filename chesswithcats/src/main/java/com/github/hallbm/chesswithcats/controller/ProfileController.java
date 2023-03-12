2d89ce668f546940f71271955b5ce4f7d1e04abe
HallBM <bhall.phd@gmail.com>
2023-03-12 19:28:31 -0400

    Update ProfileController.java
    Used @AuthenticationPrincipal to provide data for profile page instead of making a DB query

--- chesswithcats/src/main/java/com/github/hallbm/chesswithcats/controller/ProfileController.java
+++ chesswithcats/src/main/java/com/github/hallbm/chesswithcats/controller/ProfileController.java
@@ -1,21 +1,34 @@
 package com.github.hallbm.chesswithcats.controller;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.web.exchanges.HttpExchange.Principal;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.hallbm.chesswithcats.domain.GameEnums.GameStyle;
+import com.github.hallbm.chesswithcats.dto.PlayerRegistrationDTO;
 import com.github.hallbm.chesswithcats.model.Player;
 import com.github.hallbm.chesswithcats.repository.PlayerRepository;
+import com.github.hallbm.chesswithcats.service.GameServices;
+import com.github.hallbm.chesswithcats.service.PlayerServices;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @Component("ProfilePageController")
@@ -23,25 +36,32 @@ public class ProfileController {
 
 	@Autowired
 	PlayerRepository playerRepo;
-
+	
+	@Autowired
+	PlayerServices playerServ;
+	
+	@Autowired
+	GameServices gameServ;
+	
 	@GetMapping("/profile")
-	public ModelAndView accessProfile(Model model, Principal user) {
-		Player player = playerRepo.findByUsername(user.getName());
-		String fullname = player.getFirstName() + " " + player.getLastName();
-		int calcAcctDays = (int) Duration.between(player.getDateJoined(), LocalDate.now()).toDays();
+	public ModelAndView accessProfile(Model model, @AuthenticationPrincipal Player currentUser) {
+		
+		String fullname = currentUser.getFirstName() + " " + currentUser.getLastName();
+		int calcAcctDays = (int) ChronoUnit.DAYS.between(currentUser.getDateJoined(), LocalDate.now());
 		String dayDisplay = String.format(" (%d %s)", calcAcctDays, calcAcctDays == 1 ? "day" : "days");
 
-		model.addAttribute("iconFile", player.getIconFile());
-		model.addAttribute("username", player.getUsername());
-		model.addAttribute("email", player.getEmail());
+		model.addAttribute("iconFile", currentUser.getIconFile());
+		model.addAttribute("username", currentUser.getUsername());
+		model.addAttribute("email", currentUser.getEmail());
 		model.addAttribute("password", "************");
 		model.addAttribute("name", fullname);
-		model.addAttribute("dateJoined", player.getDateJoined());
+		model.addAttribute("dateJoined", currentUser.getDateJoined());
 		model.addAttribute("accountDays", dayDisplay);
 		model.addAttribute("isEditable", true);
-
-		//TODO games, scores, etc
-
+		
+		Map<GameStyle,Integer[]> dbResults = gameServ.getWinDrawLosePercentageByPlayer(currentUser.getUsername());
+		model.addAttribute("gameResults", dbResults);
+		
 		return new ModelAndView("profile", model.asMap());
 	}
 
@@ -54,7 +74,7 @@ public class ProfileController {
 		}
 
 		Player other = playerRepo.findByUsername(username);
-		int calcAcctDays = (int) Duration.between(other.getDateJoined(), LocalDate.now()).toDays();
+		int calcAcctDays = (int) ChronoUnit.DAYS.between(other.getDateJoined(), LocalDate.now());
 		String dayDisplay = String.format(" (%d %s)", calcAcctDays, calcAcctDays == 1 ? "day" : "days");
 
 		model.addAttribute("iconFile", other.getIconFile());
@@ -62,11 +82,27 @@ public class ProfileController {
 		model.addAttribute("dateJoined", other.getDateJoined());
 		model.addAttribute("accountDays", dayDisplay);
 		model.addAttribute("isEditable", false);
+		model.addAttribute("accountStatus", other.isActive()); 
 
-		//TODO games, scores, etc
+		Map<GameStyle,Integer[]> dbResults = gameServ.getWinDrawLosePercentageByPlayer(username);
+		model.addAttribute("gameResults", dbResults);
+		
+		Map<GameStyle,Integer[]> matchResults = gameServ.getWinDrawLosePercentageByOpponents(currentUser.getUsername(), username);
+		model.addAttribute("matchResults", matchResults);
 
+		model.addAttribute("online", other.isLogged()); 
+		
 		return new ModelAndView("profile");
 	}
-
-	//TODO PROFILE EDITS
+	
+	@Modifying
+	@Transactional
+	@PostMapping("/delete-account")
+	public ModelAndView deleteAccount(Model model, @AuthenticationPrincipal Player currentUser) {
+		currentUser.setActive(false);
+		playerRepo.save(currentUser);
+		return new ModelAndView("redirect:/logout");
+	}
+	
+	
 }
