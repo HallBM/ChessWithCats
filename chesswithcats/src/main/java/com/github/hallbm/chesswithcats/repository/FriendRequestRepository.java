26ebae484a6a889f69beca7cd1085701397591c1
HallBM <bhall.phd@gmail.com>
2023-03-12 19:39:01 -0400

    Update FriendRequestRepository.java
    Created Native Query for finding friend request objects between two players (independent of who sent / received)

--- chesswithcats/src/main/java/com/github/hallbm/chesswithcats/repository/FriendRequestRepository.java
+++ chesswithcats/src/main/java/com/github/hallbm/chesswithcats/repository/FriendRequestRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.github.hallbm.chesswithcats.domain.FriendEnums.BlockedBy;
 import com.github.hallbm.chesswithcats.domain.FriendEnums.FriendRequestStatus;
 import com.github.hallbm.chesswithcats.model.FriendRequest;
+import com.github.hallbm.chesswithcats.model.Game;
 
 import jakarta.transaction.Transactional;
 
@@ -26,6 +28,11 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, Lo
 	public List<FriendRequest> findByReceiverUsernameAndStatusAndBlockedBy(String receiver, FriendRequestStatus status, BlockedBy blockedBy);
 	public List<FriendRequest> findBySenderUsernameAndStatusAndBlockedBy(String sender, FriendRequestStatus status, BlockedBy blockedBy);
 
+	@Query(value = "SELECT * FROM friend_requests f "
+			+ "WHERE (f.sender_username = :username1 AND f.receiver_username = :username2) OR "
+			+ "(f.sender_username = :username2 AND f.receiver_username = :username1)", nativeQuery = true)
+	FriendRequest getFriendshipByUsernames(String username1, String username2);
+	
 	@Modifying
 	@Transactional
 	public long deleteByReceiverUsernameAndSenderUsername(String receiver, String sender);
