f668bb6cfc379fa3ca85b55a27597a6e4c1d85f0
HallBM <bhall.phd@gmail.com>
2023-03-14 13:40:06 -0400

    Update GameBoardServices.java

--- chesswithcats/src/main/java/com/github/hallbm/chesswithcats/service/GameBoardServices.java
+++ chesswithcats/src/main/java/com/github/hallbm/chesswithcats/service/GameBoardServices.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Random;
 
 import com.github.hallbm.chesswithcats.domain.GameEnums.GameStyle;
@@ -13,7 +12,7 @@ import com.github.hallbm.chesswithcats.model.Game;
 import com.github.hallbm.chesswithcats.model.GameBoard;
 
 /**
- * Services associated with maintenance and properties of the actual chessboard state.
+ * Services associated with maintenance and properties of the actual chess board state.
  */
 
 public final class GameBoardServices {
@@ -21,11 +20,15 @@ public final class GameBoardServices {
 	/**
 	 * Square positions of the corresponding PieceNotation[][] board: e.g., [0][0] = "A8".
 	 */
-	private final static String[][] squares = { { "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8" },
-			{ "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7" }, { "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6" },
-			{ "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5" }, { "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4" },
-			{ "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3" }, { "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2" },
-			{ "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1" } };
+	private final static String[][] squares = { 
+			{ "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8" },
+			{ "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7" }, 
+			{ "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6" },
+			{ "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5" }, 
+			{ "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4" },
+			{ "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3" }, 
+			{ "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2" },
+			{ "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1" }};
 
 	/**
 	 * Mapping of column letter to PieceNotation[][] board index.
@@ -248,16 +251,23 @@ public final class GameBoardServices {
 	 * Returns array of row index and column index of indicated king.
 	 */
 	public static int[] findKingPosition(PieceNotation[][] board, PieceNotation Kk) {
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				if (board[i][j] == Kk) {
-					return new int[]{i,j};
+		
+		if( Kk == PieceNotation.K || Kk == PieceNotation.k) {
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					if (board[i][j] == Kk) {
+						return new int[]{i,j};
+					}
 				}
 			}
-		}
+		} 
+		
 		return new int[]{-1, -1};
 	}
 
+	/**
+	 * Generates and returns a deep copy of a chess board PieceNotation[8][8].
+	 */
 	public static PieceNotation[][] copyBoard(PieceNotation[][] board){
 		PieceNotation[][] newBoard = new PieceNotation[8][];
 		
